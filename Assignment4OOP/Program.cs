829d234797f5c5257cc2ba9e1dabb3f683c9c979
ayman <aymanehab2002@gmail.com>
2025-01-19 06:00:13 +0200

    Q3

--- Assignment4OOP/Program.cs
+++ Assignment4OOP/Program.cs
@@ -32,16 +32,16 @@
             #endregion
 
             #region Q03
-            //ComplexNumber c1=new ComplexNumber();
-            //ComplexNumber c2=new ComplexNumber();
+            //ComplexNumber c1 = new ComplexNumber();
+            //ComplexNumber c2 = new ComplexNumber();
             //c1.Real = 2;
             //c1.Imagine = 3;
             //c2.Real = 1;
             //c2.Imagine = 2;
             //Console.WriteLine(c1);
             //Console.WriteLine(c2);
-            //Console.WriteLine(c1+c2);
-            //Console.WriteLine(c1-c2);
+            //Console.WriteLine(c1 + c2);
+            //Console.WriteLine(c1 - c2);
             #endregion
 
             #region Q04
