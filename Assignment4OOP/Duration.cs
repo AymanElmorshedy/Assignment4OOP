2e110472b01331f357f985699d77214f0f6f7cdc
ayman <aymanehab2002@gmail.com>
2025-01-19 05:55:17 +0200

    part02 Q1

---
+++ Assignment4OOP/Duration.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4OOP
+{
+    internal class Duration
+    {
+        #region Proberites
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+
+        #endregion
+        #region Constructor
+        public Duration()
+        {
+
+        }
+        public Duration(int h,int m,int s)
+        {
+            Hours = h;
+            Minutes = m;
+            Seconds = s;
+        }
+
+        public Duration(int seconds)
+        {
+            Hours = seconds / (3600);
+            Minutes = (seconds % (3600)) / 60;
+            Seconds = seconds % 60;
+        }
+        #endregion
+        #region Methods
+        public override string ToString()
+        {
+            return $"Hours : {Hours} ,Minutes : {Minutes}, Seconds : {Seconds}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Duration otherDuration)
+            {
+                return this.Hours == otherDuration.Hours &&
+                       this.Minutes == otherDuration.Minutes &&
+                       this.Seconds == otherDuration.Seconds;
+            }
+            return false;
+        }
+
+        public static bool Equals(Duration? obj1, Duration? obj2)
+        {
+            if (obj1 is null || obj2 is null)
+                return false;
+
+            return obj1.Hours == obj2.Hours &&
+                   obj1.Minutes == obj2.Minutes;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Hours, Minutes, Seconds);
+        }
+        public static Duration operator +(Duration left, int right)
+        {
+            return new Duration
+            {
+                Hours = left.Hours + right / (3600),
+                Minutes = left.Minutes + (right % (3600)) / 60,
+                Seconds = left.Seconds + right % 60
+            };
+        }
+        public static Duration operator +(int left, Duration right) => right + left;
+
+        public static Duration operator -(Duration left, Duration right)
+        {
+            return new Duration
+            {
+                Hours = left.Hours - right.Hours,
+                Minutes = left.Minutes - right.Minutes,
+                Seconds = left.Seconds - right.Seconds
+            };
+        }
+        public static Duration operator ++(Duration duration)
+        {
+            return new Duration
+            {
+                Hours = ++duration.Hours,
+                Minutes = ++duration.Minutes,
+                Seconds = ++duration.Seconds,
+            };
+        }
+        public static Duration operator --(Duration duration)
+        {
+            return new Duration
+            {
+                Hours = --duration.Hours,
+                Minutes = --duration.Minutes,
+                Seconds = --duration.Seconds,
+            };
+        }
+        public static bool operator >(Duration left, Duration right)
+        {
+            if (left.Hours * 3600 + left.Minutes * 60 + left.Seconds > right.Hours * 360 + right.Minutes * 60 + right.Seconds)
+                return true;
+            return false;
+        }
+        public static bool operator <(Duration left, Duration right)
+        {
+            if (left.Hours * 3600 + left.Minutes * 60 + left.Seconds < right.Hours * 360 + right.Minutes * 60 + right.Seconds)
+                return true;
+            return false;
+        }
+        public static bool operator >=(Duration left, Duration right)
+        {
+            if (left.Hours * 3600 + left.Minutes * 60 + left.Seconds >= right.Hours * 360 + right.Minutes * 60 + right.Seconds)
+                return true;
+            return false;
+        }
+        public static bool operator <=(Duration left, Duration right)
+        {
+            if (left.Hours * 3600 + left.Minutes * 60 + left.Seconds <= right.Hours * 360 + right.Minutes * 60 + right.Seconds)
+                return true;
+            return false;
+        }
+        public static explicit operator bool(Duration? obj)
+        {
+            if (obj is not null) return true;
+            else return false;
+        }
+        public static explicit operator DateTime(Duration? obj)
+        {
+            return new DateTime(obj.Hours, obj.Minutes, obj.Seconds);
+        }
+
+
+        #endregion
+    }
+}
--- Assignment4OOP/Program.cs
+++ Assignment4OOP/Program.cs
@@ -52,26 +52,63 @@
             #endregion
 
             #region Q05
-            BaseClass baseClass = new BaseClass();
-            baseClass.Print();
+            //BaseClass baseClass = new BaseClass();
+            //baseClass.Print();
 
-            BaseClass base2 = new DrivedClass1();
-            base2.Print();//dynamic polymorphism -dynamic override-dynamic binding -late binding
-                          //runtime 
-                          //This myfun02 from child typeB and A :10
-                          //MyFun02() viryual method overrided using (override)
-                          //resolve on run time based on object type
+            //BaseClass base2 = new DrivedClass1();
+            //base2.Print();//dynamic polymorphism -dynamic override-dynamic binding -late binding
+            //              //runtime 
+            //              //This myfun02 from child typeB and A :10
+            //              //MyFun02() viryual method overrided using (override)
+            //              //resolve on run time based on object type
 
-            BaseClass base3 = new DrivedClass2();
-            base3.Print();//static polymorphism -static binding-early binding vased on referance type
-                          //compilation time
-                          //MyFun01 method non virtual overide using(new)
-                          //resolve in compile time based on referance type
+            //BaseClass base3 = new DrivedClass2();
+            //base3.Print();//static polymorphism -static binding-early binding vased on referance type
+            //              //compilation time
+            //              //MyFun01 method non virtual overide using(new)
+            //              //resolve in compile time based on referance type
 
 
             #endregion
 
+            #endregion
 
+            #region Part02
+            Duration dur = new Duration(1, 2, 3);
+            Duration dur2 = new Duration(1, 2, 4);
+
+            Console.WriteLine(dur.GetHashCode());
+
+            if (dur.Equals(dur2)) Console.WriteLine("True");
+            else Console.WriteLine("false");
+
+            Duration d1 = new Duration(1, 10, 15);
+            d1.ToString();
+
+            Duration d2 = new Duration(3600);
+            d2.ToString();
+
+            Duration d3 = new Duration(7800);
+            d3.ToString();
+
+            Duration d4 = new Duration(666);
+            d4.ToString();
+            Console.WriteLine("**************************************************");
+            d3.ToString();
+            d3 = d1 + 7800;
+            d3.ToString();
+            d3 = 666 + d3;
+            d3.ToString();
+            d3 = ++d1;
+            d3.ToString();
+            d3 = --d2;
+            d3.ToString();
+            d1 = d1 - d2;
+            d1.ToString();
+            if (d1 > d2) Console.WriteLine("D1 > D2");
+            else Console.WriteLine("D2 > D1");
+            if (d1 <= d2) Console.WriteLine("D2 >= D2");
+            else Console.WriteLine("D1 >= D2");
             #endregion
         }
     }
