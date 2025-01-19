303c8a27809d711f84f3473971a7748b9381300b
ayman <aymanehab2002@gmail.com>
2025-01-19 04:03:43 +0200

    Q1

---
+++ Assignment4OOP/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4OOP
+{
+    internal class Calculator
+    {
+        #region Prpperites
+        //public int Number01 { get; set; }
+        //public int Number02 { get; set; }
+
+        //public int Number03 { get; set; }
+
+        //public double Number04 { get; set; }
+
+        //public double Number05 { get; set; }
+
+
+        #endregion
+        #region Methods
+        public  int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public  int Sum(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+        public  double Sum(double x, double y)
+        {
+            return x + y;
+        }
+            
+        #endregion
+    }
+}
--- Assignment4OOP/Program.cs
+++ Assignment4OOP/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part01
+            #region Q1
+            //Calculator calculator = new Calculator();
+            //calculator.Number01 = 10;
+            //calculator.Number02 = 10;
+            //calculator.Number03 = 10;
+            //calculator.Number04 = 2.5;
+            //calculator.Number05 = 2.5;
+            //calculator.sum(calculator.Number01,calculator.Number02);
+            //calculator.sum(calculator.Number01,calculator.Number02,calculator.Number03);
+            //calculator.sum(calculator.Number04,calculator.Number02);
+            int a=10,b=20,c=30;
+            double x = 2.5, y = 2.5;
+            Calculator calculator = new Calculator();
+            Console.WriteLine(calculator.Sum(a, b, c));
+            Console.WriteLine(calculator.Sum(x, y));
+            Console.WriteLine(calculator.Sum(a,b));
+
+            #endregion
+            #endregion
         }
     }
 }
