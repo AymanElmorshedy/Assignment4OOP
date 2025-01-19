5642161b15be37234825c0a3ea7c1c8a39b3fa87
ayman <aymanehab2002@gmail.com>
2025-01-19 04:34:10 +0200

    Q03

---
+++ Assignment4OOP/ComplexNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4OOP
+{
+    internal class ComplexNumber
+    {
+        #region Properites
+        public int Real { get; set; }
+        public int Imagine { get; set; }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"{Real}+{Imagine}i";
+        }
+        public static ComplexNumber operator +(ComplexNumber left, ComplexNumber right)
+        {
+            return new ComplexNumber()
+            {
+               Real= (left?.Real ??0) + (right?.Real ??0),
+               Imagine= (left?.Imagine??0) + (right?.Imagine ??0),
+            };
+        }
+
+        public static ComplexNumber operator -(ComplexNumber left, ComplexNumber right)
+        {
+            return new ComplexNumber()
+            {
+                Real = (left?.Real ?? 0) - (right?.Real ?? 0),
+                Imagine = (left?.Imagine ?? 0) - (right?.Imagine ?? 0),
+            };
+        }
+        #endregion
+    }
+}
--- Assignment4OOP/Program.cs
+++ Assignment4OOP/Program.cs
@@ -6,26 +6,42 @@
         {
             #region Part01
             #region Q1
+
+            //int a=10,b=20,c=30;
+            //double x = 2.5, y = 2.5;
             //Calculator calculator = new Calculator();
-            //calculator.Number01 = 10;
-            //calculator.Number02 = 10;
-            //calculator.Number03 = 10;
-            //calculator.Number04 = 2.5;
-            //calculator.Number05 = 2.5;
-            //calculator.sum(calculator.Number01,calculator.Number02);
-            //calculator.sum(calculator.Number01,calculator.Number02,calculator.Number03);
-            //calculator.sum(calculator.Number04,calculator.Number02);
-            int a=10,b=20,c=30;
-            double x = 2.5, y = 2.5;
-            Calculator calculator = new Calculator();
-            Console.WriteLine(calculator.Sum(a, b, c));
-            Console.WriteLine(calculator.Sum(x, y));
-            Console.WriteLine(calculator.Sum(a,b));
+            //Console.WriteLine(calculator.Sum(a, b, c));
+            //Console.WriteLine(calculator.Sum(x, y));
+            //Console.WriteLine(calculator.Sum(a,b));
 
             #endregion
 
             #region Q2
+            //Rectangel rectangel1 = new Rectangel();
+            //Console.WriteLine(rectangel1.Height);
+            //Console.WriteLine(rectangel1.Width);
+            //Console.WriteLine(*****************************************);
+            //Rectangel rectangel2 = new Rectangel(1,2);
+            //Console.WriteLine(rectangel2.Height);
+            //Console.WriteLine(rectangel2.Width);
+            //Console.WriteLine(******************************************);
+            //Rectangel rectangel3 = new Rectangel(3);
+            //Console.WriteLine(rectangel3.Height);
+            //Console.WriteLine(rectangel3.Width);
 
+            #endregion
+
+            #region Q03
+            ComplexNumber c1=new ComplexNumber();
+            ComplexNumber c2=new ComplexNumber();
+            c1.Real = 2;
+            c1.Imagine = 3;
+            c2.Real = 1;
+            c2.Imagine = 2;
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(c1+c2);
+            Console.WriteLine(c1-c2);
             #endregion
             #endregion
         }
